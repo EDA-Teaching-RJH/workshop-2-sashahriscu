9dc9a5c90c0ae81e1daa365a9c66807914361113
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-04 10:46:00 +0100

    workshop 2 task 1 completion

--- partOne.py
+++ partOne.py
@@ -1,8 +1,19 @@
+# task: implement a program that prompts the user for input and then outputs that same input replacing each space with three periods
+
+# define principal function
 def main():
+# prompt user to input a line of text
     slow = input("Input ")
     replace(slow)
 
+# define function to replace the whitespaces
 def replace(text):
-  #Your code goes here.
+# strip function to remove whitespaces in the input string
+  slow = text.strip()
+# replace the blank spaces in input with 3 periods
+  result = slow.replace(" ", "...")
+# print the output. output being whitespace replaced with three periods
+  print(result)
 
+# call on main to run function
 main()
