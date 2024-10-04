04cffaa8e595d5c5c03bc0a73434823d6c395d87
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-04 10:50:42 +0100

    completion

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,19 @@
-import math  
+import math
 
+# define principal function
 def main():
-#TO DO  
+# prompt user input for sides A and B, float used for decimal numbers
+    A = float(input("What is the length of side A? "))
+    B = float(input("What is the length of side B? "))
+# define 'C' or the hypotenuse
+    pythag(A,B)
+# print result of the hypotenuse
+    print(f"The length of the hypotenuse is: {(pythag(A,B))}")
 
+# define result
 def pythag(A,B):
-#TO DO  
-
+# square rooting to return C result
+    return math.sqrt((A**2) + (B**2))
+    
+# call on main to perform function
 main()
